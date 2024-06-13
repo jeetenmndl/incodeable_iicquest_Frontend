089ac76db197fa9058d9b2cd84e5f034f0b59868
jeeten848 <jeetenmandal848@gmail.com>
2024-06-13 23:40:01 +0545

    final submission

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -23,38 +23,8 @@ const page = () => {
 
         <section className='py-8 lg:py-12 grid grid-cols-1 lg:grid-cols-2 gap-4'>
             <div>
-                {/* <h2 className='text-2xl font-semibold sticky top-8'>About Pariwar</h2> */}
-                <section className="grid grid-cols-1 lg:grid-cols-3 px-6 lg:px-0 gap-6 [&>div]:rounded-md">
-        
-        <div className="border lg:h-96 p-5 lg:p-3 bg-green-100">
-          <h3 className="font-medium text-2xl">Build a virtual Family</h3>
-        <p className="text-xs text-gray-600 pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum alias rem perspiciatis delectus earum dolorem iusto praesentium facere</p>
-        <hr className="border-blue-200 mt-5" />
-        <div className="w-full px-4 py-8">
-          <Image src={Card1} className="w-full " alt="card1" />
-        </div>
-        </div>
-
-
-        <div className="border lg:h-96 p-5 lg:p-3 bg-blue-100">
-          <h3 className="font-medium text-2xl">Share Thoughts & Problems</h3>
-        <p className="text-xs text-gray-600 pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum alias rem perspiciatis delectus earum dolorem iusto praesentium facere</p>
-        <hr className="border-blue-200 mt-5" />
-        <div className="w-full px-8 py-4">
-          <Image src={Card2} className="w-full " alt="card1" />
-        </div>
-        </div>
-        
-        <div className="border lg:h-96 p-5 lg:p-3 bg-orange-100">
-          <h3 className="font-medium text-2xl">Track Down the Resolution</h3>
-        <p className="text-xs text-gray-600 pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum alias rem perspiciatis delectus earum dolorem iusto praesentium facere</p>
-        <hr className="border-blue-200 mt-5" />
-        <div className="w-full px-8 py-4">
-          <Image src={Card3} className="w-full " alt="card1" />
-        </div>
-        </div>
-
-      </section>
+                <h2 className='text-2xl font-semibold sticky top-8'>About Pariwar</h2>
+                
             </div>
             <article>
                 <p>There are students who know they can solve their problems by discussing with their specific family members. Yet they can’t share their feelings and problems because of various reasons leading to mental health issues. Create and build a web relation that we are in need of.</p>
